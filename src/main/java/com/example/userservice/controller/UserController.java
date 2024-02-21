a9771204de89cee12e7b88334f527a21acdf31a6
Sushant Poman <sushantpoman94@gmail.com>
2024-02-21 20:05:32 +0530

    Added Sleuth for MS tracing

--- src/main/java/com/example/userservice/controller/UserController.java
+++ src/main/java/com/example/userservice/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.userservice.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.orderservice.controller.OrderController;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.model.User;
 import com.example.userservice.service.UserService;
 
 @RestController
 public class UserController {
+	
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
 	
 	@Autowired
 	UserService userService;
@@ -66,6 +72,7 @@ public class UserController {
 	
 	@GetMapping("/orderByName/{name}")
 	public ResponseEntity<UserDto> getOrdersByUsername(@PathVariable String name) {
+		log.info("From UserService");
 		return new ResponseEntity<UserDto>(userService.getOrders(name), HttpStatus.OK);
 
 	}
